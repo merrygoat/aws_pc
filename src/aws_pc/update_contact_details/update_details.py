ae41f27a96acd6fde351a92f3bf3d0cb6ac84188
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2024-06-27 15:36:16 +0100

    Finish update contact details script

--- src/aws_pc/update_contact_details/update_details.py
+++ src/aws_pc/update_contact_details/update_details.py
@@ -1,29 +1,23 @@
-from typing import Type
+from typing import Type, Optional
 
 import boto3
 import botocore.client
+import tqdm
 import yaml
 
 from aws_pc import organization
 
 
-def audit_alternate_contact(account_client: Type[botocore.client.BaseClient],
-                            management_account_id: str, account_id: str, contact_type: str):
-    if account_id != management_account_id:
-        return account_client.get_contact_information(AccountId='string')
-    else:
-        return account_client.get_contact_information()
+def get_alternate_contact_info(account_client: Type[botocore.client.BaseClient], account_id: str,
+                               contact_type: str) -> Optional[dict]:
+    try:
+        response = account_client.get_alternate_contact(AccountId=account_id, AlternateContactType=contact_type)
+    except account_client.exceptions.ResourceNotFoundException:
+        return None
+    return response["AlternateContact"]
 
 
-def change_contact_details(account_client: Type[botocore.client.BaseClient], management_account_id: str,
-                           account_id: str, contact_details: dict):
-    if account_id != management_account_id:
-        account_client.put_contact_information(AccountId=account_id, ContactInformation=contact_details)
-    else:
-        account_client.put_contact_information(ContactInformation=contact_details)
-
-
-def loop_over_accounts(sso_profile_name: str):
+def loop_over_accounts(sso_profile_name: str, update_details: bool = False):
     """Loop through all accounts in organization updating the contact information."""
 
     with open("details.yaml", 'r') as input_file:
@@ -35,15 +29,31 @@ def loop_over_accounts(sso_profile_name: str):
 
     org_client = session.client('organizations')
     accounts = organization.get_organisation_accounts(org_client, include_suspended=False)
-
     account_client = session.client('account')
-    accounts = [{"Id": "463829754490"}]
-    for account in accounts:
-        # change_contact_details(account_client, management_account_id, account["Id"], contact_details)
-        details = audit_alternate_contact(account_client, management_account_id, account["Id"], "BILLING")
-        print(f"{account['Id']}: {details}")
+
+    contact_info = {}
+    for account in tqdm.tqdm(accounts):
+        account_info = {}
+        account_id = account["Id"]
+
+        account_info["default"] = account_client.get_contact_information(AccountId=account_id)["ContactInformation"]
+        account_info["billing"] = get_alternate_contact_info(account_client, account_id, "BILLING")
+        account_info["security"] = get_alternate_contact_info(account_client, account_id, "SECURITY")
+        account_info["operations"] = get_alternate_contact_info(account_client, account_id, "OPERATIONS")
+
+        contact_info[account_id] = account_info
+        if update_details:
+            account_client.put_contact_information(AccountId=account_id, ContactInformation=contact_details)
+
+    with open("contact_details_report.txt", 'w') as output_file:
+        output_file.write(f"Report on the contact details of all accounts in the organization managed by the "
+                          f"management account id: '{management_account_id}'\n")
+        for account_id, account_details in contact_info.items():
+            output_file.write(f"Contact info for account: {account_id}\n")
+            for contact_type, details in account_details.items():
+                output_file.write(f"{contact_type}: {details}\n\n")
 
 
 if __name__ == "__main__":
-    loop_over_accounts("management-hrds")
+    loop_over_accounts("management-hrds", update_details=True)
 
